72e86aa14131fd57994554378cb7262ee930af1a
giselars <gisela-rs@outlook.com>
2023-01-13 15:37:20 +0100

    :sparkles: add calendar actions

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,23 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { localizer, getMessegesES } from '../../helpers';
 import { Navbar, CalendarEvent, CalendarModal, FabAddNew, FabDelete } from "../";
-import { useUiStore, useCalendarStore } from '../../hooks';
+import { useUiStore, useCalendarStore, useAuthStore } from '../../hooks';
 
 
 export const CalendarPage = () => {
 
+  const { user } = useAuthStore();
   const { openDateModal } = useUiStore();
-  const { events, setActiveEvent } = useCalendarStore();
+  const { events, setActiveEvent, startLoadingEvents } = useCalendarStore();
   const [ lastView, setLastView ] = useState(localStorage.getItem('lastView') || 'week' );
 
   const eventStyleGetter = ( event, start, end, isSelected ) => {
     
+    const isMyEvent = ( user.uid === event.user._id ) || ( user.uid === event.user.uid );
 
     const style = {
-        backgroundColor: '#347CF7',
+        backgroundColor: isMyEvent ? '#347CF7' : '#464660',
         borderRadius: '0px',
         opacity: 0.8,
         color: 'white'
@@ -42,6 +44,11 @@ export const CalendarPage = () => {
     localStorage.setItem( 'lastView', event );
     setLastView( event );
   }
+
+  useEffect(() => {
+    startLoadingEvents();
+  }, [])
+  
 
   return (
     <>
