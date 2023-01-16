beee44b13f59c70a0d4f9c3a92879330b451c406
giselars <gisela-rs@outlook.com>
2023-01-16 14:41:05 +0100

    :wrench: add config files and :white_check_mark: slice tests

---
+++ tests/__fixtures/authStates.js
@@ -0,0 +1,20 @@
+export const initialState = {
+    status: 'checking',
+    user: {},
+    errorMessage: undefined,
+}
+
+export const authenticatedState = {
+    status: 'authenticated',
+    user: {
+        uid: 'abc',
+        name: 'Nombre'
+    },
+    errorMessage: undefined,
+}
+
+export const notAuthenticatedState = {
+    status: 'not-authenticated',
+    user: {},
+    errorMessage: undefined,
+}
