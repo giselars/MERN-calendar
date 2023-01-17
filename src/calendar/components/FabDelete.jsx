477f8dea441b492c772be6ead94b06f251ad7827
giselars <gisela-rs@outlook.com>
2023-01-17 11:36:34 +0100

    :test_tube: add custom hooks tests, router and components tests

--- src/calendar/components/FabDelete.jsx
+++ src/calendar/components/FabDelete.jsx
@@ -13,11 +13,12 @@ export const FabDelete = () => {
 
   return (
     <button
-     className="btn btn-danger fab-danger"
-     onClick={ handleDelete }
-     style={{
-      display: hasEventSelected ? '' : 'none'
-     }}
+      aria-label="btn-delete"
+      className="btn btn-danger fab-danger"
+      onClick={ handleDelete }
+      style={{
+        display: hasEventSelected ? '' : 'none'
+      }}
     >
         <i className="fa fa-trash-alt"></i>
     </button>
